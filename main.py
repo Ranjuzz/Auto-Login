f2936ff1ed252c62572f3fa949e509b8ebf001f5
Ranjith <Ranjiththangavel11@gmail.com>
2024-04-04 00:22:47 +0530

    Comment lines added

--- main.py
+++ main.py
@@ -10,10 +10,9 @@ driver = webdriver.Chrome(service=service)
 
 driver.get("http://10.10.32.1:2280/cportal")
 driver.maximize_window();
-# time.sleep(2)
 driver.switch_to.frame("login_win")
 
-username = "21ec128"
+username = "21ec128" 
 password = "123456"
 
 usrid = "usrname"
@@ -21,7 +20,7 @@ passid = "newpasswd"
 submit = "update_btn"
 driver.find_element(By.ID, usrid).send_keys(username)
 driver.find_element(By.ID, passid).send_keys(password)
-# time.sleep(2)
+time.sleep(2)
 driver.find_element(By.ID, submit).submit()
-# time.sleep(2)
+time.sleep(2)
 driver.quit()
